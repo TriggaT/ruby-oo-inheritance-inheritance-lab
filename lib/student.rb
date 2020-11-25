72735b0a021c969f089a17d5f6cb88926296f3b7
Trayshawn <traytwebb14@yahoo.com>
2020-11-25 15:15:02 -0600

    commit tree 16c972391ea5cd6e0d97ffa840521a6a1e9bb646

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+    attr_accessor :knowledge 
+
+    @@all = []
+
+    def initialize
+        @knowledge = []
+    end 
+
+    def learn(string)
+        @knowledge << string
+    end 
+
+    def knowledge
+        @knowledge
+    end 
 
 end
